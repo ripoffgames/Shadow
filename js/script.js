8296c627c23bd021d201c0e857b0720b77528498
Shreyas Lad <dieppoopshooter@gmail.com>
2016-08-12 11:38:40 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// browser window scroll (in pixels) after which the "menu" link is shown
+var offset = 300;
+
+var navigationContainer = $('#cd-nav'),
+	mainNavigation = navigationContainer.find('#cd-main-nav ul');
+	
+$(window).scroll(function(){
+	checkMenu();
+});
+ 
+function checkMenu() {
+ 
+	if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+		//add .is-fixed class to #cd-nav 
+		//wait for the animation to end  
+		//add the .has-transitions class to main navigation (used to bring back transitions)
+	} else if ($(window).scrollTop() <= offset) {
+		
+		//check if the menu is open when scrolling up - for browser that supports transition
+		if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+			//close the menu 
+			//wait for the transition to end 
+			//remove the .is-fixed class from the #cd-nav and the .has-transitions class from main navigation
+		} 
+ 
+		//check if the menu is open when scrolling up - fallback if transitions are not supported
+		else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+			//no need to wait for the end of transition - close the menu and remove the .is-fixed class from the #cd-nav
+		} 
+ 
+		//scrolling up with menu closed
+		else {
+			//remove the .is-fixed class from the #cd-nav and the .has-transitions class from main navigation
+		}
+	} 
+}
